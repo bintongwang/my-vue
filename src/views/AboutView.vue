44636753d6e32fdafee86912de51b7b17aabb0fb
w30033178 <871372581@qq.com>
2022-07-17 17:58:22 +0800

    sdfsf

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    sdsdfsdfgi
-    dsfdsfsdf
-    dsfdsfsdf
-    dsfdsfsdf
+    sdsdfsdfgi dsfdsfsdf dsfdsfsdf dsfdsfsdf
     <h1>This is an about page</h1>
     <h1>This is an about page</h1>
     <h1>This is an about page</h1>
