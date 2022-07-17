4f47f469268aed422835e5e4c1de974e4180a7eb
w30033178 <871372581@qq.com>
2022-07-17 16:26:57 +0800

    firstTest

---
+++ src/components/Dsdf.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <ul></ul>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,5 +14,21 @@ export default {
   components: {
     HelloWorld,
   },
+  mounted() {
+    // this.init();
+  },
+  methods: {
+    // init() {
+    //   setTimeout(() => {
+    //     console.log(145);
+    //     setTimeout(() => {
+    //       console.log(2);
+    //       setTimeout(() => {
+    //         console.log(3);
+    //       }, 1000);
+    //     }, 1000);
+    //   }, 1000);
+    // },
+  },
 };
 </script>
